08b122a06e98f07522bf1839db6cc0e9e9640f8d
sentfromthehub <121725874+sentfromthehub@users.noreply.github.com>
2023-10-18 15:25:46 -0400

    Add sidebar panels for UI

--- app.R
+++ app.R
@@ -2,7 +2,36 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+  
+  # Application title
+  titlePanel("eGFR Calculator"),
+  
+  # Sidebar with inputs for eGFR calculation
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("creatinine", 
+                   "Creatinine Level: (µmol/L)", 
+                   value = 1, 
+                   min = 0.5, 
+                   max = 15),
+      numericInput("age", 
+                   "Age:", 
+                   value = 30, 
+                   min = 18, 
+                   max = 100),
+      selectInput("sex", 
+                  "Sex:", 
+                  choices = c("Female", "Male")),
+      selectInput("race", 
+                  "Race:", 
+                  choices = c("Non-Black", "Black"))
+    ),
+    
+    # Show the eGFR calculation
+    mainPanel(
+      textOutput("eGFR")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram

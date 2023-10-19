8af1bfe6c564323ffe76c89291bea8e3a734a5d0
sentfromthehub <121725874+sentfromthehub@users.noreply.github.com>
2023-10-19 14:17:50 -0400

    Remove race-based input

--- app.R
+++ app.R
@@ -22,9 +22,6 @@ ui <- fluidPage(
       selectInput("sex", 
                   "Sex:", 
                   choices = c("Female", "Male")),
-      selectInput("race", 
-                  "Race:", 
-                  choices = c("Non-Black", "Black"))
     ),
     
     # Show the eGFR calculation
@@ -36,7 +33,28 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output) {
-
+  output$eGFR <- renderText({
+    # Determine the constants based on sex
+    if (input$sex == "Female") {
+      kappa <- 0.7
+      alpha <- -0.241
+      sex_constant <- 1.012
+    } else {
+      kappa <- 0.9
+      alpha <- -0.302
+      sex_constant <- 1
+    }
+    
+    # Calculate the minimum and maximum of Scr/kappa or 1.0
+    min_value <- min(input$creatinine/kappa, 1)
+    max_value <- max(input$creatinine/kappa, 1)
+    
+    # Calculate eGFR
+    eGFR <- 142 * min_value^alpha * max_value^-1.200 * 0.9938^input$age * sex_constant
+    
+    # Return the eGFR as a string
+    paste("eGFR:", round(eGFR, 2))
+  })
 }
 
 # Run the application 
